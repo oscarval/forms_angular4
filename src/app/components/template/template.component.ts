291c5a509a965da8317af5e9d0cab2e480ae10e9
Alexis Valdez <oscarval@ucm.es>
2018-06-04 18:20:53 +0200

    Control and error message with ngForm, ngModel.

--- src/app/components/template/template.component.ts
+++ src/app/components/template/template.component.ts
@@ -1,15 +1,30 @@
 import { Component, OnInit } from '@angular/core';
+import { NgForm } from '@angular/forms';
 
 @Component({
   selector: 'app-template',
   templateUrl: './template.component.html',
-  styles: []
+  styles: [`
+    .ng-invalid.ng-touched:not(form) {
+      border: 1px solid red;
+    }
+  `]
 })
 export class TemplateComponent implements OnInit {
+
+  usuario: Object = {
+    nombre: null,
+    apellido : null,
+    email: ''
+  };
 
   constructor() { }
 
   ngOnInit() {
   }
 
+  guardar( forma: NgForm ) {
+  console.log('ngform', forma);
+  console.log('valor', forma.value);
+  }
 }
